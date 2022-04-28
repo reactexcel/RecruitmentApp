78489b4552a278c0566c147320bf43413be2c164
khatipnkj <pankajkhati@excellencetechnologies.in>
2022-04-28 11:38:35 +0530

    Animated api added

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -4,18 +4,48 @@ import {
   View,
   TextInput,
   TouchableOpacity,
+  Animated,
+  Easing
 } from 'react-native';
-import React, {useState} from 'react';
+import React, {useState,useRef, useEffect} from 'react';
 
 const Login = ({navigation}) => {
   const [email, setEmail] = useState('');
+  const logAnim = useRef(new Animated.Value(0)).current;
   const LoginAuth = () => {
     if (email == '0007') {
       navigation.navigate('Home');
     }
   };
+  useEffect(() =>{
+    spin()
+  } ,[])
+  const spin = () => {
+    logAnim.setValue(0)
+    Animated.timing(
+      logAnim,{
+        toValue:1,
+        duration:10000,
+        easing: Easing.linear
+      }
+    ).start(() => {spin()})
+  }
+
+  
+  const Ghum = logAnim.interpolate({
+    inputRange:[0,1],
+    outputRange:['0deg','360deg']
+  })
   return (
     <View style={styles.container}>
+      <Animated.Image  
+                    style={{ 
+                      marginBottom:12, 
+                        width: 227,  
+                        height: 200,  
+                        transform: [{rotate: Ghum}] }}  
+                    source={require('../assests/unnamed.jpg')}  
+                />  
       <TextInput
         placeholder="Enter your Email"
         style={styles.inputEmail}
